856b2c75e0566b8cf870679cf0662da83caacbd8
GGNoWayBack <2522402609@qq.com>
2023-09-10 14:07:19 +0800

    fix: Post-processing of data records

--- cathodedataextractor/relationextractpostprocessing.py
+++ cathodedataextractor/relationextractpostprocessing.py
@@ -40,74 +40,44 @@ def data_pprocess(data):
     else:
         if data[binary_columns[1]] is None:
             # Capacity, cycle split if no cycle, retention extracted
-            repeat = set()
-            for Cycle, Capacity in data[binary_columns[0]]:
-                if Cycle not in repeat:
-                    new_ = deepcopy(data)
-                    new_.pop('Cycle_capacity: mAhg-1')
-                    new_.pop('Cycle_retention: %')
-                    new_['Cycle'] = Cycle
-                    new_['Capacity'] = Capacity
-                    res_json.append(new_)
-                repeat.add(Cycle)
+            cycle, capacity = data[binary_columns[0]][0]
+            new_ = deepcopy(data)
+            new_.pop('Cycle_capacity: mAhg-1')
+            new_.pop('Cycle_retention: %')
+            new_['Cycle'] = cycle
+            new_['Capacity'] = capacity
+            res_json.append(new_)
         elif data[binary_columns[0]] is None:
-            for Cycle, Retention in data[binary_columns[1]]:
-                if Cycle > 1:
-                    new_ = deepcopy(data)
-                    new_.pop('Cycle_capacity: mAhg-1')
-                    new_.pop('Cycle_retention: %')
-                    new_['Cycle'] = Cycle
-                    new_['Retention'] = Retention
-                    res_json.append(new_)
-                else:
-                    print(data)
-        else:  # If both are extracted and there is a first lap capacity.
+            cycle, retention = data[binary_columns[1]][0]
+            if cycle > 1:
+                new_ = deepcopy(data)
+                new_.pop('Cycle_capacity: mAhg-1')
+                new_.pop('Cycle_retention: %')
+                new_['Cycle'] = cycle
+                new_['Retention'] = retention
+                res_json.append(new_)
+
+        else:  # If both are extracted and there is a first cycle capacity.
             # Convert the retention rate to the corresponding number of cycles according to the first cycle,
             # and also save the capacity retention rate, generating two pieces of data.
-            count = 0
             cca_cra = [data.pop('Cycle_capacity: mAhg-1'), data.pop('Cycle_retention: %')]
-            c_c = {}
-            for cycle_capacity in cca_cra[0]:
-                if cycle_capacity[0] == 1:
-                    count += 1
-                if count > 1:
-                    break
-                if cycle_capacity[0] not in c_c:
-                    c_c[cycle_capacity[0]] = cycle_capacity[1]
-            if count == 1:
-                for cycle_retention in cca_cra[1]:
-                    if cycle_retention[0] not in c_c:
-                        c_c[cycle_retention[0]] = round(c_c[1] * cycle_retention[1] / 100, 1)
-                for Cycle, Capacity in cca_cra[0]:
-                    new_ = deepcopy(data)
-                    new_['Cycle'] = Cycle
-                    new_['Capacity'] = Capacity
-                    res_json.append(new_)
-                for Cycle, Retention in cca_cra[1]:
-                    new_ = deepcopy(data)
-                    new_['Cycle'] = Cycle
-                    new_['Retention'] = Retention
-                    res_json.append(new_)
-            elif count == 0:  # If the number of cycle is the same,
+            if cca_cra[0][0][0] == 1:
+                cca_cra[0].append([cca_cra[1][0][0], round(cca_cra[1][0][1] * cca_cra[0][0][1] / 100, 1)])
+            elif cca_cra[0][0] == cca_cra[1][0]:  # If the number of cycle is the same,
                 # then convert to the first cycle capacity and generate three pieces of data.
-                for Cycle, Capacity in cca_cra[0]:
-                    new_ = deepcopy(data)
-                    new_['Cycle'] = Cycle
-                    new_['Capacity'] = Capacity
-                    res_json.append(new_)
-                for Cycle, Retention in cca_cra[1]:
-                    new_ = deepcopy(data)
-                    new_['Cycle'] = Cycle
-                    new_['Retention'] = Retention
-                    res_json.append(new_)
-                if cca_cra[0][0][0] == cca_cra[1][0][0]:
-                    new_capacity = round(cca_cra[0][0][1] * 100 / cca_cra[1][0][1], 1)
-                    new_ = deepcopy(data)
-                    new_['Cycle'] = 1
-                    new_['Capacity'] = new_capacity
-                    res_json.append(new_)
-            else:
-                res_json.append(data)
+                cca_cra[0].append([1, round(cca_cra[0][0][1] * 100 / cca_cra[1][0][1], 1)])
+
+            for cycle, capacity in cca_cra[0]:
+                new_ = deepcopy(data)
+                new_['Cycle'] = cycle
+                new_['Capacity'] = capacity
+                res_json.append(new_)
+            for cycle, retention in cca_cra[1]:
+                new_ = deepcopy(data)
+                new_['Cycle'] = cycle
+                new_['Retention'] = retention
+                res_json.append(new_)
+
     for _, final in enumerate(res_json):
         res_json[_] = dict(sorted(final.items(), key=lambda x: _sort.get(x[0], 12)))
     return res_json
